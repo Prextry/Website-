b91e6451872439ad99658a2ff042c7694f2632c2
Prextry <prexofficial1@gmail.com>
2026-04-03 11:56:01 +0530

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <iframe 
+      src="/index.html" 
+      style={{
+        width: '100%',
+        height: '100vh',
+        border: 'none',
+        margin: 0,
+        padding: 0,
+        overflow: 'hidden'
+      }}
+      title="Taqueria Los Cuates"
+    />
+  )
+}
